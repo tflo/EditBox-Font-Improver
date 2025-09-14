0498bbd91ff5a84a902aa9406e4ac0696ae2ce95
6mot, Tom <tflo@users.noreply.github.com>
2025-09-14 19:45:12 +0200

    SPDX-License-Identifier, readme update

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+-- SPDX-License-Identifier: GPL-3.0-or-later
+-- Copyright (c) 2022-2025 Thomas Floeren
 
 local me, A = ...
 local db
@@ -223,25 +225,3 @@ ef:SetScript("OnEvent", on_event)
 -- 	elseif msg == "runcmd" then
 -- 	end
 -- end
-
-
---[[ License ===================================================================
-
-	Copyright © 2022-2025 Thomas Floeren
-
-	This file is part of EditBox Font Improver.
-
-	EditBox Font Improver is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by the
-	Free Software Foundation, either version 3 of the License, or (at your
-	option) any later version.
-
-	EditBox Font Improver is distributed in the hope that it will be useful, but
-	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-	or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-	more details.
-
-	You should have received a copy of the GNU General Public License along with
-	EditBox Font Improver. If not, see <https://www.gnu.org/licenses/>.
-
-============================================================================]]--
